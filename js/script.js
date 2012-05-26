b1f9a1fd7877166651c63717bf4c47b56656d066
Michael Myers <michael.morris.myers@gmail.com>
2012-05-25 22:00:15 -0400

    layout good, content additions

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
-/* Author:
+/*  script.js
+ *
+ * @license Copyright (C) 2012, Atomizer Software
+ * http://atomizersoft.net
+ * @author Atomizer Software
+ *
+ *
+ */
 
-*/
-
-
-
+$(function () {
+    $.deck('.slide');
+});
 
 
