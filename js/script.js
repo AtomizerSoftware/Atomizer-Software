70c26b374c380deb5f0e273652888c0f8647b74e
Michael Myers <michael.morris.myers@gmail.com>
2012-05-30 00:10:12 -0400

    background opacity mostly work, not in IE or with arrows

--- js/script.js
+++ js/script.js
@@ -7,8 +7,40 @@
  *
  */
 
-$(function () {
+function locationHashChanged() {
+
+    if (location.hash === "#welcome" || null) {
+        console.log(location.hash);
+        $('#background').removeClass("translucent");
+
+    } else {
+        $('#background').addClass("translucent");
+        console.log(location.hash);
+    }
+}
+
+window.onhashchange = locationHashChanged;
+
+
+$(document).ready(function () {
     $.deck('.slide');
+
+    //check hash initial conditions
+    if (location.hash === "#welcome" || null) {
+        console.log(location.hash);
+        $('#background').removeClass("translucent");
+
+    } else {
+        $('#background').addClass("translucent");
+        console.log(location.hash);
+    }
+
+    if ("onhashchange" in window) {
+       console.log("The browser supports the hashchange event!");
+    }
+
+    console.log(location.hash);
+
 });
 
 
